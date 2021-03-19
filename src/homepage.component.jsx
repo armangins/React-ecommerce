8db28a4526717bef0c76871170701abd652fefbc
armangins <armangins@gmail.com>
2021-03-19 16:32:14 +0200

    add sass

---
+++ src/homepage.component.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import './homepage.styles.scss'
+
+const homePage = () => (
+    <div className="homepage">
+        <div className="main-menu">
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">כובעים</h1>
+                    <span>מעבר לקניה</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">חולצות</h1>
+                    <span>מעבר לקניה</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">מכנסים</h1>
+                    <span>מעבר לקניה</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">מעילים</h1>
+                    <span>מעבר לקניה</span>
+                </div>
+            </div>
+            <div className="menu-item">
+                <div className="content">
+                    <h1 className="title">נעליים</h1>
+                    <span>מעבר לקניה</span>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+
+export default homePage
