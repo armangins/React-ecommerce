ca9e34f5e6cab48fcb53ff021d580bbc933fd36d
armangins <armangins@gmail.com>
2021-07-28 22:19:48 +0300

    add logger only to the dev

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,8 +4,11 @@ import rootReducer from "./root-reducer";
 import { persistStore,} from "redux-persist";
 
 
+const middlewares = [];
+if(process.env.NODE_ENV == "development"){
+    middlewares.push(logger)
+}
 
-const middlewares = [logger];
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 export const persistor = persistStore(store);
 // export default { store, persistor };
